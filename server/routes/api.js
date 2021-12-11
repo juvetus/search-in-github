71e0488cf90c80f0303a4cafa29d46b32aaa37db
Juvet <juvettaj@gmail.com>
2021-12-11 14:11:40 +0100

    first commit

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,7 +12,7 @@ router.get("/github/userinfo/:user", async function (req, res) {
       "User-Agent":
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36",
     },
-    OAUth: "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",
+    OAUth: "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",
   };
   https
     .get(options, function (apiResponse) {
